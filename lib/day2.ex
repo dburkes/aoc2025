2e110574a9920299f9376334eabaee2a3cbbd289
Danny Burkes <dburkes@netable.com>
2025-12-01 22:58:27 -0800

    Day 2 Part 2, quite inefficiently (for now)

--- lib/day2.ex
+++ lib/day2.ex
@@ -3,23 +3,51 @@ defmodule Day2 do
 
   def part1 do
     parse_ranges()
-    |> Enum.map(&find_invalid_ids_in_range/1)
+    |> Enum.map(fn range ->
+      find_invalid_ids_in_range(range, false)
+    end)
     |> List.flatten()
     |> Enum.sum()
   end
 
-  def find_invalid_ids_in_range(range) do
+  def part2 do
+    parse_ranges()
+    |> Enum.map(fn range ->
+      find_invalid_ids_in_range(range, true)
+    end)
+    |> List.flatten()
+    |> Enum.sum()
+  end
+
+  def find_invalid_ids_in_range(range, multiple_repeats) do
     Enum.filter(range, fn id ->
-      invalid_id?(Integer.digits(id))
+      invalid_id?(Integer.digits(id), multiple_repeats)
     end)
   end
 
-  def invalid_id?(digits) when Integer.is_odd(length(digits)) do
+  def invalid_id?(digits, multiple_repeats \\ false) do
+    invalid?(digits, multiple_repeats)
+  end
+
+  def invalid?(digits, _) when length(digits) == 1 do
     false
   end
 
-  def invalid_id?(digits) when Integer.is_even(length(digits)) do
+  def invalid?(digits, false) when Integer.is_odd(length(digits)) do
+    false
+  end
+
+  def invalid?(digits, false) when Integer.is_even(length(digits)) do
     Enum.take(digits, div(length(digits), 2)) == Enum.take(digits, -div(length(digits), 2))
+  end
+
+  def invalid?(digits, true) do
+    1..div(length(digits), 2)
+    |> Enum.any?(fn size ->
+      Enum.chunk_every(digits, size)
+      |> Enum.uniq()
+      |> length() == 1
+    end)
   end
 
   defp parse_ranges do
--- test/day2_test.exs
+++ test/day2_test.exs
@@ -1,9 +1,9 @@
 defmodule Day2Test do
   use ExUnit.Case
 
-  describe "invalid ID detection" do
+  describe "invalid ID detection without multiple repeats" do
     test "odd length ID is valid" do
-      assert Day2.invalid_id?([1, 2, 3]) == false
+      assert Day2.invalid_id?([1, 1, 1]) == false
     end
 
     test "even length ID with matching halves is invalid" do
@@ -15,9 +15,44 @@ defmodule Day2Test do
     end
   end
 
+  describe "invalid ID detection with multiple repeats" do
+    test "single digit ID is valid" do
+      assert Day2.invalid_id?([1], true) == false
+    end
+
+    test "odd length ID with all same digits is invalid" do
+      assert Day2.invalid_id?([2, 2, 2, 2, 2], true) == true
+    end
+
+    test "odd length ID with differing digits is valid" do
+      assert Day2.invalid_id?([2, 2, 3, 2, 1], true) == false
+    end
+
+    test "odd length ID with multiple repeats is invalid" do
+      assert Day2.invalid?([1, 2, 3, 1, 2, 3, 1, 2, 3], true) == true
+    end
+
+    test "even length ID with matching halves is invalid" do
+      assert Day2.invalid_id?([3, 4, 3, 4], true) == true
+    end
+
+    test "even length ID with non-matching halves is valid" do
+      assert Day2.invalid_id?([3, 4, 5, 6], true) == false
+    end
+
+    test "even length ID with multiple repeats is invalid" do
+      assert Day2.invalid?([2, 1, 2, 1, 2, 1, 2, 1, 2, 1], true) == true
+    end
+
+  end
+
   describe "AOC quizes" do
     test "part1" do
       assert Day2.part1() == 13919717792
+    end
+
+    test "part2" do
+      assert Day2.part2() == 14582313461
     end
   end
 end
