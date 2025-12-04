34296a15153b655073b7a9abba2626cb2b383fdf
Danny Burkes <dburkes@netable.com>
2025-12-04 11:48:47 -0800

    Use MapSet to improve performance

--- lib/day4.ex
+++ lib/day4.ex
@@ -1,106 +1,52 @@
 defmodule Day4 do
   def part1 do
-    parse_grid()
-    |> count_accessible_rolls()
+    rolls = parse_rolls()
+    Enum.count(rolls, &roll_free?(rolls, &1))
   end
 
   def part2 do
-    grid = parse_grid()
-    count_rolls(grid) - count_rolls(evolve_all(grid))
+    rolls = parse_rolls()
+
+    cleaned =
+      Stream.repeatedly(fn -> [] end)
+      |> Enum.reduce_while(rolls, fn _, acc ->
+        removable =
+          Enum.filter(acc, &roll_free?(acc, &1))
+          |> MapSet.new()
+
+        case MapSet.difference(acc, removable) do
+          ^acc -> {:halt, acc}
+          remaining -> {:cont, remaining}
+        end
+      end)
+
+    MapSet.size(rolls) - MapSet.size(cleaned)
   end
 
-  def count_rolls(grid) do
-    Enum.reduce(grid, 0, fn row, acc ->
-      acc + Enum.count(String.graphemes(row), fn cell -> cell == "@" end)
-    end)
-  end
-
-  def count_accessible_rolls(grid) do
-    neighbor_counts(grid)
-    |> List.flatten()
-    |> Enum.count(fn n -> n < 4 end)
-  end
-
-  def evolve_one(grid) do
-    neighbor_counts(grid)
-    |> Enum.map_reduce(0, fn row, row_num ->
-      {
-        Enum.map_reduce(row, 0, fn cell, col_num ->
-          orig_cell = String.at(Enum.at(grid, row_num), col_num)
-
-          new_cell =
-            cond do
-              cell < 4 ->
-                "."
-
-              true ->
-                orig_cell
-            end
-
-          {new_cell, col_num + 1}
-        end)
-        |> elem(0) |> Enum.join(),
-        row_num + 1
-      }
-    end)
-    |> elem(0)
-  end
-
-  def evolve_all(grid) do
-    new_grid = evolve_one(grid)
-
-    if new_grid == grid do
-      new_grid
-    else
-      evolve_all(new_grid)
+  defp adjacent_positions({x, y}) do
+    for dx <- -1..1, dy <- -1..1, dx != 0 or dy != 0 do
+      {x + dx, y + dy}
     end
   end
 
-  def neighbor_counts(grid) do
-    rows = length(grid)
-    cols = String.length(Enum.at(grid, 0))
-
-    Enum.map_reduce(grid, 0, fn row, row_num ->
-      {
-        String.graphemes(row)
-        |> Enum.map_reduce(0, fn _cell, col_num ->
-          {count_neighbors(grid, row_num, col_num, rows, cols), col_num + 1}
-        end)
-        |> elem(0),
-        row_num + 1
-      }
-    end)
-    |> elem(0)
+  def roll_free?(rolls, pos) do
+    pos
+    |> adjacent_positions()
+    |> Enum.count(&MapSet.member?(rolls, &1))
+    |> Kernel.<(4)
   end
 
-  def count_neighbors(grid, row, col, rows, cols) do
-    cell = String.at(Enum.at(grid, row), col)
-    count_neighbors(cell, grid, row, col, rows, cols)
-  end
-
-  def count_neighbors(cell, _grid, _row, _col, _rows, _cols) when cell != "@" do
-    nil
-  end
-
-  def count_neighbors(_cell, grid, row, col, rows, cols) do
-    directions = [{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}]
-
-    Enum.reduce(directions, 0, fn {dr, dc}, acc ->
-      acc + neighbor_val(grid, row + dr, col + dc, rows, cols)
-    end)
-  end
-
-  defp neighbor_val(_grid, row, col, rows, cols)
-       when row < 0 or row >= rows or col < 0 or col >= cols do
-    0
-  end
-
-  defp neighbor_val(grid, row, col, _rows, _cols) do
-    if String.at(Enum.at(grid, row), col) == ".", do: 0, else: 1
-  end
-
-  defp parse_grid do
+  def parse_rolls do
     File.stream!("lib/fixtures/day4.txt")
     |> Enum.map(&String.trim/1)
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, row} ->
+      line
+      |> String.to_charlist()
+      |> Enum.with_index()
+      |> Enum.filter(&(elem(&1, 0) == ?@))
+      |> Enum.map(&{elem(&1, 1), row})
+    end)
+    |> MapSet.new()
   end
 end
--- test/day4_test.exs
+++ test/day4_test.exs
@@ -1,59 +1,30 @@
 defmodule Day4Test do
   use ExUnit.Case
 
-  @grid [
-    ".@..",
-    "@@@.",
-    ".@.."
-  ]
-  @grid_rows length(@grid)
-  @grid_cols String.length(Enum.at(@grid, 0))
+  describe "free roll detection" do
+    test "works" do
+      # @.@.
+      # @@@@
+      # @.@.
+      grid = MapSet.new([
+        {0, 0},
+        {0, 2},
+        {1, 0},
+        {1, 1},
+        {1, 2},
+        {1, 3},
+        {2, 0},
+        {2, 2}
+      ])
 
-  @gen2_grid [
-    "....",
-    ".@..",
-    "...."
-  ]
-
-  @gen3_grid [
-    "....",
-    "....",
-    "...."
-  ]
-
-  describe "neighbor counting" do
-    test "with all eight neighbors" do
-      assert Day4.count_neighbors(@grid, 1, 1, @grid_rows, @grid_cols) == 4
-    end
-
-    test "on the first row" do
-      assert Day4.count_neighbors(@grid, 0, 1, @grid_rows, @grid_cols) == 3
-    end
-
-    test "on the last row" do
-      assert Day4.count_neighbors(@grid, 2, 1, @grid_rows, @grid_cols) == 3
-    end
-
-    test "on the first column" do
-      assert Day4.count_neighbors(@grid, 1, 0, @grid_rows, @grid_cols) == 3
-    end
-
-    test "on the last column" do
-      assert Day4.count_neighbors(@grid, 1, 2, @grid_rows, @grid_cols) == 3
-    end
-  end
-
-  describe "map evolution" do
-    test "produces the next generation correctly" do
-      assert Day4.evolve_one(@grid) == @gen2_grid
-    end
-
-    test "produces stable generation" do
-      assert Day4.evolve_one(@gen2_grid) == @gen3_grid
-    end
-
-    test "stops evolving when stable" do
-      assert Day4.evolve_all(@grid) == @gen3_grid
+      assert Day4.roll_free?(grid, {0, 0})
+      assert Day4.roll_free?(grid, {0, 2})
+      assert Day4.roll_free?(grid, {1, 0})
+      refute Day4.roll_free?(grid, {1, 1})
+      refute Day4.roll_free?(grid, {1, 2})
+      assert Day4.roll_free?(grid, {1, 3})
+      assert Day4.roll_free?(grid, {2, 0})
+      assert Day4.roll_free?(grid, {2, 2})
     end
   end
 
