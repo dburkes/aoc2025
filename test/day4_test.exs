92884bf5ed021acc445c84db86f57b62dbbf6776
Danny Burkes <dburkes@netable.com>
2025-12-04 20:29:07 -0800

    fix x/y mixup in tests

--- test/day4_test.exs
+++ test/day4_test.exs
@@ -8,22 +8,22 @@ defmodule Day4Test do
       # @.@.
       grid = MapSet.new([
         {0, 0},
-        {0, 2},
-        {1, 0},
-        {1, 1},
-        {1, 2},
-        {1, 3},
         {2, 0},
+        {0, 1},
+        {1, 1},
+        {2, 1},
+        {3, 1},
+        {0, 2},
         {2, 2}
       ])
 
       assert Day4.roll_free?(grid, {0, 0})
-      assert Day4.roll_free?(grid, {0, 2})
-      assert Day4.roll_free?(grid, {1, 0})
-      refute Day4.roll_free?(grid, {1, 1})
-      refute Day4.roll_free?(grid, {1, 2})
-      assert Day4.roll_free?(grid, {1, 3})
       assert Day4.roll_free?(grid, {2, 0})
+      assert Day4.roll_free?(grid, {0, 1})
+      refute Day4.roll_free?(grid, {1, 1})
+      refute Day4.roll_free?(grid, {2, 1})
+      assert Day4.roll_free?(grid, {3, 1})
+      assert Day4.roll_free?(grid, {0, 2})
       assert Day4.roll_free?(grid, {2, 2})
     end
   end
