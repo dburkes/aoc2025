49b0da0698f4a2c907a2fba4b3ce053b336038a9
Danny Burkes <dburkes@netable.com>
2025-12-12 00:05:07 -0800

    day 12 part 1 (actually)

--- lib/day12.ex
+++ lib/day12.ex
@@ -1,2 +1,85 @@
 defmodule Day12 do
+  def part1() do
+    parse()
+    |> count_fits()
+  end
+
+  def count_fits(data) do
+    Map.get(data, :regions)
+    |> Enum.map(fn region ->
+      if crudely_fits?(region, Map.get(data, :shapes)), do: 1, else: 0
+    end)
+    |> Enum.sum()
+  end
+
+  def crudely_fits?(region, shapes) do
+    shape = Enum.at(shapes, 1)
+    area_per_shape = length(shape) * String.length(Enum.at(shape, 0))
+
+    area_needed = Enum.sum(Map.get(region, :counts)) * area_per_shape
+
+    area_available = Map.get(region, :width) * Map.get(region, :height)
+
+    area_available >= area_needed
+  end
+
+  def parse(input \\ File.read!("lib/fixtures/day12.txt")) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.chunk_while(
+      [],
+      fn line, acc ->
+        case line do
+          "" ->
+            {:cont, acc, []}
+
+          _ ->
+            {:cont, [line | acc]}
+        end
+      end,
+      &{:cont, &1, []}
+    )
+    |> List.flatten()
+    |> Enum.reverse()
+    |> Enum.reduce(%{:shapes => [], :regions => []}, fn chunk, acc ->
+      case chunk =~ ": " do
+        true ->
+          String.split(chunk, "\n", trim: true)
+          |> Enum.reduce(acc, fn
+            line, inner_acc ->
+              fields = Regex.run(~r/(\d+)x(\d+): (.+)$/, line)
+
+              width = Enum.at(fields, 1)
+              height = Enum.at(fields, 2)
+
+              counts =
+                Enum.at(fields, 3)
+                |> String.split()
+                |> Enum.map(&String.to_integer/1)
+
+              region = %{
+                :width => String.to_integer(width),
+                :height => String.to_integer(height),
+                :counts => counts
+              }
+
+              Map.merge(inner_acc, %{:regions => [region | Map.get(inner_acc, :regions)]})
+          end)
+          |> then(fn inner_acc -> Map.merge(acc, inner_acc) end)
+
+        false ->
+          String.split(chunk, "\n", trim: true)
+          |> Enum.slice(1..-1//1)
+          |> then(fn shape_data ->
+            Map.merge(acc, %{:shapes => [shape_data | Map.get(acc, :shapes)]})
+          end)
+      end
+    end)
+    |> then(fn data ->
+      %{
+        :shapes => Enum.reverse(Map.get(data, :shapes)),
+        :regions => Enum.reverse(Map.get(data, :regions))
+      }
+    end)
+  end
 end
--- test/day12_test.exs
+++ test/day12_test.exs
@@ -2,15 +2,59 @@ defmodule Day11Test do
   use ExUnit.Case
 
   @input """
-  aaa: you hhh
-  you: bbb ccc
-  bbb: ddd eee
-  ccc: ddd eee fff
-  ddd: ggg
-  eee: out
-  fff: out
-  ggg: out
-  hhh: ccc fff iii
-  iii: out
+  0:
+  ###
+  ##.
+  ##.
+
+  1:
+  ###
+  ##.
+  .##
+
+  2:
+  .##
+  ###
+  ##.
+
+  3:
+  ##.
+  ###
+  ##.
+
+  4:
+  ###
+  #..
+  ###
+
+  5:
+  ###
+  .#.
+  ###
+
+  4x4: 0 0 0 0 2 0
+  12x5: 1 0 1 0 2 2
+  12x5: 1 0 1 0 3 2
   """
+
+  test "parsing" do
+    data = Day12.parse(@input)
+    shapes = Map.get(data, :shapes)
+    regions = Map.get(data, :regions)
+
+    assert length(shapes) == 6
+    assert length(regions) == 3
+
+    shape = Enum.at(shapes, 1)
+    assert shape == ["###", "##.", ".##"]
+
+    region = Enum.at(regions, 0)
+    assert Map.get(region, :width) == 4
+    assert Map.get(region, :height) == 4
+    assert Map.get(region, :counts) == [0, 0, 0, 0, 2, 0]
+  end
+
+  test "part1" do
+    assert Day12.part1() == 569
+  end
 end
